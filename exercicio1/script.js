7691631403ed4023ad9cb21d565053c08016b4f4
Jessica <jessica.thamiresnunes@gmail.com>
2022-08-29 17:22:11 -0300

    Respondido exercícios 1

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+//let nome
+//let idade
+//console.log(nome, idade)
+let nome = prompt ("Qual é o seu nome?")
+let idade = Number (prompt("Quantos anos você tem?"))
+console.log("Olá", nome, "você tem", idade)
+
+//console.log (typeof nome, typeof idade)
