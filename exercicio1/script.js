38e8c22863b092c1311037492de06839d8ef64c6
Jessica <jessica.thamiresnunes@gmail.com>
2022-08-29 19:00:02 -0300

    Exercícios 2 e 3 realizados

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -3,6 +3,7 @@
 //console.log(nome, idade)
 let nome = prompt ("Qual é o seu nome?")
 let idade = Number (prompt("Quantos anos você tem?"))
-console.log("Olá", nome, "você tem", idade)
+console.log("Olá", nome, "você tem", idade, "anos.")
 
-//console.log (typeof nome, typeof idade)
+
+console.log (typeof nome, typeof idade)
---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+let a = 10
+let b = 25
+let auxiliar = a
+a=b
+b=auxiliar
+console.log( "Novo valor de a", a)
+console.log("Novo valor de b", b)
